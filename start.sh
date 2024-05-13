de5fff6a92c1f6c4da04e106e3406df94bd76141
Luciano Branco <thelucianobranco@gmail.com>
2024-05-13 02:49:24 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,3 +6,5 @@ stage1=/media/internatl/downloads/PPLGPwn-main/stage1.bin
 stage2=/media/internatl/downloads/PPLGPwn-main/stage2.bin
 chmod +x ./pppwn
 	./pppwn --interface $interface --fw $firmware --stage1 $stage1 --stage2 $stage2
+# send nofification
+luna-send -a webosbrew -f -n 1 luna://com.webos.notification/createToast '{"sourceId":"webosbrew","message": "<b>PPLGPwn!</b><br/>Starting your Jailbreak. by: llbranco."}'
