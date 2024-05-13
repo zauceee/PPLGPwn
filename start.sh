63f5b5d150d85d9155ead5c59c23b83254597f9f
zauce <37784801+zauceee@users.noreply.github.com>
2024-05-13 09:14:49 +0100

    Atualizar o start.sh

--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@ echo "PPLGPWN - Designed for lgwebtvs! Ported by Kodeine, with luv <3"
 echo "updated by llbranco"
 read -p "Which interface: " int
 read -p "Firmware ver(750-1100): " fmv
-stage1=/media/internatl/downloads/PPLGPwn-main/stage1.bin
-stage2=/media/internatl/downloads/PPLGPwn-main/stage2.bin
+stage1=/media/internal/downloads/PPLGPwn-main/stage1.bin
+stage2=/media/internal/downloads/PPLGPwn-main/stage2.bin
 # echo $int, $fmv, $st1, $st2
 chmod +x ./pppwn
 	./pppwn --interface $int --fw $fmv --stage1 $stage1 --stage2 $stage2
