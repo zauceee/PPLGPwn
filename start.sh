e2d88d6b1d84c659727e88bf8cb269f6386f14d3
Luciano Branco <thelucianobranco@gmail.com>
2024-05-13 02:22:56 -0300

    fw1100

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 echo "PPLGPWN - Designed for lgwebtvs! Ported by Kodeine, with luv <3"
-read -p "Which interface: " int
-read -p "Firmware ver(750-1100): " fmv
-read -p "Stage(1): " st1
-read -p "Stage(2): " st2
-# echo $int, $fmv, $st1, $st2
+echo "updated by llbranco"
+interface=eth0
+firmware=1100
+stage1=/media/internatl/downloads/PPLGPwn-main/stage1.bin
+stage2=/media/internatl/downloads/PPLGPwn-main/stage2.bin
 chmod +x ./pppwn
-	./pppwn --interface $int --fw $fmv --stage1 $st1 --stage2 $st2
+	./pppwn --interface $interface --fw $firmware --stage1 $stage1 --stage2 $stage2
