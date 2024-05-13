d1d5281e28b23de128c28dea6c8cda48a6842cde
Luciano Branco <thelucianobranco@gmail.com>
2024-05-13 03:48:51 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@ echo "PPLGPWN - Designed for lgwebtvs! Ported by Kodeine, with luv <3"
 echo "updated by llbranco"
 interface=eth0
 firmware=1100
-stage1=/media/internatl/downloads/PPLGPwn-main/stage1.bin
-stage2=/media/internatl/downloads/PPLGPwn-main/stage2.bin
+stage1=/media/internal/downloads/PPLGPwn-main/stage1.bin
+stage2=/media/internal/downloads/PPLGPwn-main/stage2.bin
 chmod +x ./pppwn
 	./pppwn --interface $interface --fw $firmware --stage1 $stage1 --stage2 $stage2
 # send nofification
