52ae36803d0a9268246f5838060ff025bcf712e1
Luciano Branco <thelucianobranco@gmail.com>
2024-05-15 02:31:21 -0300

    Delete install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-luna-send -f -n 1 luna://com.webos.notification/createToast '{"message": "<b>PPLGPwn Downloading...</b><br/>Project by<br/>zauceee and llbranco."}'
-sleep 3
-
-echo "Downloading..."
-
-test -f install2.sh && rm install2.sh
-wget https://raw.githubusercontent.com/llbranco/PPLGPwn/master/install2.sh
-chmod +x ./install2.sh
-./install2.sh
