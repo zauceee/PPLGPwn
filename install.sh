880ca71070e98666108d9330b36854cc44906205
Luciano Branco <thelucianobranco@gmail.com>
2024-05-15 02:05:52 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,6 @@
-echo -e "
-PPLGPwn llbranco mod for PS4 firmware 11.00 only
-beta installer
-"
-echo "PPLGPWN - Designed for LG WebOS TVs! Ported by Kodeine, with luv <3"
-echo "updated by llbranco"
+luna-send -f -n 1 luna://com.webos.notification/createToast '{"message": "<b>PPLGPwn Downloading...</b><br/>Project by: <br/>zauceee and llbranco."}'
+echo downloading 
 
-luna-send -f -n 1 luna://com.webos.notification/createToast '{"message": "<b>PPLGPwn Installing!</b><br/>Project by: <br/>zauceee and llbranco."}'
-
-cd /media/internal/downloads
-wget https://github.com/llbranco/PPLGPwn/archive/refs/heads/main.zip
-unzip main.zip
-cd PPLGPwn-main
-chmod +x ./start.sh
-
-echo 'PPLGPwn by llbranco' >> /var/lib/webosbrew/startup.sh
-echo 'to remove PPLGPwn from boot, remove the next line' >> /var/lib/webosbrew/startup.sh
-echo './media/internal/downloads/PPLGPwn-main/start.sh' >> /var/lib/webosbrew/startup.sh
-
-luna-send -f -n 1 luna://com.webos.notification/createToast '{"message": "<b>PPLGPwn Installed! Starting...</b><br/>The First Time is Unforgettable."}'
-./start.sh
+wget https://raw.githubusercontent.com/llbranco/PPLGPwn/master/install2.sh
+chmod +x ./install2.sh
+./install2.sh
