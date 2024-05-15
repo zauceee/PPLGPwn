90a39d48f017f7201d39bac2452bb11cdad878fe
Luciano Branco <thelucianobranco@gmail.com>
2024-05-15 01:44:14 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 echo 'just a test message'
-luna-send -f -n 1 luna://com.webos.notification/createToast '{"message": "<b>PPLGPwn test message!</b><br/>Project by: <br/>zauceee and llbranco."}'
+luna-send -f -n 1 luna://com.webos.notification/createToast '{"message": "<b>PPLGPwn test message</b><br/>Project by: <br/>zauceee and llbranco."}'
