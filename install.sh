7727690a2c9776377ce7ef6fe7107f572f837ae7
Luciano Branco <thelucianobranco@gmail.com>
2024-05-15 02:20:58 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
-#!/bin/sh
-luna-send -f -n 1 luna://com.webos.notification/createToast '{"message": "<b>PPLGPwn Downloading...</b><br/>Project by: <br/>zauceee and llbranco."}'
-echo "downloading..."
+luna-send -f -n 1 luna://com.webos.notification/createToast '{"message": "<b>PPLGPwn Downloading...</b><br/>Project by<br/>zauceee and llbranco."}'
+sleep 3
 
+echo "Downloading..."
+
+test -f install2.sh && rm install2.sh
 wget https://raw.githubusercontent.com/llbranco/PPLGPwn/master/install2.sh
 chmod +x ./install2.sh
 ./install2.sh
