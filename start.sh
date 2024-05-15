989c2d92ae1cb4e939ddcf902de599fa7692b682
Luciano Branco <thelucianobranco@gmail.com>
2024-05-15 01:58:42 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,8 +6,10 @@ stage1=/media/internal/downloads/PPLGPwn-main/stage1.bin
 stage2=/media/internal/downloads/PPLGPwn-main/stage2.bin
 /media/internal/downloads/PPLGPwn-main
 chmod +x ./pppwn
-	./pppwn --interface $interface --fw $firmware --stage1 $stage1 --stage2 $stage2
+
 # send nofification
 luna-send -f -n 1 luna://com.webos.notification/createToast '{"message": "<b>PPLGPwn!</b><br/>Iniciando PS4 Jailbreak. <br/>by: llbranco."}'
 
-# source: https://www.webosose.org/docs/reference/ls2-api/com-webos-notification/
+./pppwn --interface $interface --fw $firmware --stage1 $stage1 --stage2 $stage2 --auto-retry
+
+luna-send -f -n 1 luna://com.webos.notification/createToast '{"message": "<b>PPLGPwn!</b><br/>PS4 Pwned. <br/>by: llbranco."}'
